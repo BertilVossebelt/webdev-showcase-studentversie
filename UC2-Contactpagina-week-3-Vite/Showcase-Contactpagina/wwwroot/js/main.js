c734412d8184fec1ecc285f64496c07b214f301f
AJ Vossebelt <52101469+BertilVossebelt@users.noreply.github.com>
2025-02-13 13:28:51 +0100

    updated validation, added disable logic for submit button

--- UC2-Contactpagina-week-3-Vite/Showcase-Contactpagina/wwwroot/js/main.js
+++ UC2-Contactpagina-week-3-Vite/Showcase-Contactpagina/wwwroot/js/main.js
@@ -1,1 +1,30 @@
-console.log("hallo");
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+//
+// let resolver = (resolve, reject) => {
+//     setTimeout(() => {
+//         console.log("5")
+//         reject();
+//     }, 1000);
+// }
+//
+// let print5Promise = new Promise(resolver);
+// print5Promise.then(() => {
+//     console.log("6")
+// }).catch((err) => {
+//         consol.error(err);
+// });
+//
+// async function print56() {
+//     await print5Promise;
+//     console.log("6");
+// }
+//
+// async function main() {
+//     print56();
+// }
+//
+//
+// main();
